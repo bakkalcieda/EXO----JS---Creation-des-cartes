b325200e50b714b173eb0ee5c65add3d16c0b841
BAKKALCI Eda <stg482043@stg-bf.be>
2026-04-09 13:21:27 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,11 @@
+/*button.addEventListener('click', function() {
+    const myDiv = document.createElement('div');
+    myDiv.textContent = `Bonjour les amis`;
+    myDiv.classList.add('card');
+    myApp.appendChild(myDiv);
+  }) */
+
+  function creerCarte() {
+    const nom = document.querySelector("#nom").value
+    const age = document.querySelector("#age").value
+    const url = document.querySelector('#url').value
